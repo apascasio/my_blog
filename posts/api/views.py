55b4c424b09015aa08a39a3e868a7546122a6084
Andres Pascasio <andrespascasio@gmail.com>
2021-12-01 11:26:50 +0000

    viewset and get_post_by_Id integration added

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,21 +1,42 @@
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 
-class PostApiView(APIView):
-    def get(self, request):
+class PostApiViewSet(ViewSet):
+    def list(self, request):
         # posts= Post.objects.all()
         # posts = [post.title for post in Post.objects.all()]
         posts = PostSerializer(Post.objects.all(), many=True)
         return Response(status= status.HTTP_200_OK, data= posts.data)
 
-    def post(self, request):
+    def create(self, request):
         post = PostSerializer(data=request.POST)
         post.is_valid(raise_exception=True)
         post.save()
         return Response(status= status.HTTP_200_OK, data= post.data)
+    
+    def retrieve(self, request, pk: int):
+        try:
+            post = PostSerializer(Post.objects.get(pk= pk))
+            return Response(status= status.HTTP_200_OK, data= post.data)
+        except Post.DoesNotExist:
+            return Response(status= status.HTTP_404_NOT_FOUND, data= 'Dato no encontrado')
+
+# class PostApiView(APIView):
+#     def get(self, request):
+#         # posts= Post.objects.all()
+#         # posts = [post.title for post in Post.objects.all()]
+#         posts = PostSerializer(Post.objects.all(), many=True)
+#         return Response(status= status.HTTP_200_OK, data= posts.data)
+
+#     def post(self, request):
+#         post = PostSerializer(data=request.POST)
+#         post.is_valid(raise_exception=True)
+#         post.save()
+#         return Response(status= status.HTTP_200_OK, data= post.data)
         
         # title=request.POST.get('title', "otro")
         # description=request.POST.get('description',"sin descripcion")
